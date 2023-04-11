fe42cd454e5cb92db729f2d144b59760b34fcaf3
DanielAonicesei <aoni.daniel@gmail.com>
2023-04-11 14:10:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const key = "f4937055d11145334e71a0997a12edfc";
+
+const select = document.querySelector(".form-select");
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const icon = document.querySelector(".icon");
+const wheatherType = document.querySelector(".wheather-type");
+const time = document.querySelector(".time");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+const atmp = document.querySelector(".atmp");
+
+
+const kelvinToCelsius = (degrees) => {
+    const celsius = degrees - 273.15;
+    return celsius.toFixed(1);
+};
+
+const getCities = () =>{
+    fetch('cities.json', {
+    method: 'GET',
+  })
+.then(response => response.json())
+.then(data => {
+    data.forEach(city => {
+        const option= document.createElement("option");
+        option.value=city.id;
+        option.text=city.name;
+
+        if(city.id === 2618425){
+            option.setAttribute("selected", true);
+        }
+
+        select.appendChild(option);
+    });
+    
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
+}
+
+const getWheather = (cityId = 2618425) =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${key}`, {
+        method: 'GET',
+      })
+    .then(response => response.json())
+    .then(data => {
+        const date = new Date();
+
+        city.innerText = data.name;
+        temp.innerText = kelvinToCelsius(data.main.temp) + '\xB0C';
+        icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        wheatherType.innerText = data.weather[0].description;
+        time.innerText = `${date.toLocaleTimeString()}, ${date.toLocaleDateString()}`;
+        wind.innerText = `Wind ${data.wind.speed} m/s`;
+        humidity.innerText = `Humidity ${data.main.humidity} %`;
+        atmp.innerText = `Pressure ${data.main.pressure} hPa`;
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+getCities();
+getWheather();
+
+select.addEventListener("change", function(e) {
+    const cityId = e.target.value;
+    getWheather(cityId);
+})
